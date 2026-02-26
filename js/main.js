8c41a159887229cb2bc28f7e63bf4b4a4f1d3e29
eliorocha <e.rocha1204@gmail.com>
2026-02-25 20:06:40 -0500

    in class exercises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+
+function fullName(firstName, lastName) {
+    return firstName + lastName;
+}
+
+function avg(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[0]
+    }
+    return sum / numbers.length;
+}
+
+function space(int) {
+    let s = "";
+    for (let i = 0; i < int; i++) {
+        s += " "
+        j = Number(i) + 1;
+        console.log(s + j);
+    }
+}
+
+space(20);
